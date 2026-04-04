b952138f7ea0fcad39773fcc1878560bbb547cb0
Matthieu <matthieudeniau1@gmail.com>
2026-04-04 11:03:44 -0400

    modification de la db

--- app/src/main/java/com/example/table/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/table/data/local/AppDatabase.kt
@@ -15,7 +15,7 @@ import com.example.table.domain.model.IngredientEntity
         MealEntity::class,
         IngredientEntity::class
     ],
-    version = 2,
+    version = 1,
     exportSchema = false
 )
 @TypeConverters(
@@ -29,11 +29,5 @@ abstract class AppDatabase : RoomDatabase() {
 
     companion object {
         const val DATABASE_NAME = "mealplanner.db"
-
-        val MIGRATION_1_2 = object : Migration(1, 2) {
-            override fun migrate(database: SupportSQLiteDatabase) {
-                database.execSQL("ALTER TABLE ingredients ADD COLUMN quantity TEXT NOT NULL DEFAULT ''")
-            }
-        }
     }
 }
